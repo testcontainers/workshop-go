a1c0a4ed1b180aee49c38541b46de107c4091790
Manuel de la Peña <mdelapenya@gmail.com>
2023-10-18 13:15:14 +0200

    feat: implement POST handler to add a rating

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/testcontainers/workshop-go/internal/ratings"
 )
 
 func Root(c *gin.Context) {
@@ -12,10 +13,41 @@ func Root(c *gin.Context) {
 	})
 }
 
+// AddRating is the handler for the `POST /ratings` endpoint.
+// It will add a new rating to the store, where the rating is read from the JSON payload
+// using the following format:
+//
+//	{
+//	  "talk_uuid": "123",
+//	  "rating": 5
+//	}
 func AddRating(c *gin.Context) {
-	c.HTML(http.StatusOK, "ratings-add.tmpl", gin.H{})
+	ratingsRepo, err := ratings.NewRepository(c, Connections.Ratings)
+	if err != nil {
+		handleError(c, err)
+		return
+	}
+
+	var rating ratings.Rating
+	err = c.ShouldBind(&rating)
+	if err != nil {
+		handleError(c, err)
+		return
+	}
+
+	ratingsRepo.Add(c, rating)
+
+	c.HTML(http.StatusOK, "ratings-add.tmpl", gin.H{
+		"rating": rating,
+	})
 }
 
 func Ratings(c *gin.Context) {
 	c.HTML(http.StatusOK, "ratings-list.tmpl", gin.H{})
+}
+
+func handleError(c *gin.Context, err error) {
+	c.HTML(http.StatusInternalServerError, "error.tmpl", gin.H{
+		"message": err.Error(),
+	})
 }
