0072daff1a285cd539f86755585f57c3900cc950
Manuel de la Peña <mdelapenya@gmail.com>
2023-10-23 12:27:45 +0200

    chore: rename struct

--- internal/app/metadata.go
+++ internal/app/metadata.go
@@ -5,13 +5,13 @@ import "os"
 // The connection string for each of the services needed by the application.
 // The application will need them to connect to services, reading it from
 // the right environment variable in production, or from the container in development.
-type connections struct {
+type Metadata struct {
 	Ratings string `json:"ratings"` // Read from the RATINGS_CONNECTION environment variable
 	Streams string `json:"streams"` // Read from the STREAMS_CONNECTION environment variable
 	Talks   string `json:"talks"`   // Read from the TALKS_CONNECTION environment variable
 }
 
-var Connections *connections = &connections{
+var Connections *Metadata = &Metadata{
 	Ratings: os.Getenv("RATINGS_CONNECTION"),
 	Streams: os.Getenv("STREAMS_CONNECTION"),
 	Talks:   os.Getenv("TALKS_CONNECTION"),
