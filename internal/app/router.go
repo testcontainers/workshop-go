9c45550820f531c00b3b021cc106964cc4455bcf
Manuel de la Peña <mdelapenya@gmail.com>
2025-05-07 13:06:12 +0200

    chore: add default logger to fiber app

--- internal/app/router.go
+++ internal/app/router.go
@@ -2,10 +2,13 @@ package app
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func SetupApp() *fiber.App {
 	app := fiber.New()
+
+	app.Use(logger.New())
 
 	app.Get("/", Root)
 	app.Get("/ratings", Ratings)
